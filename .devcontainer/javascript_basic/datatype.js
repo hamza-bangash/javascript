291d7629fa5b6b2a84b3be90938d72c82748c1bd
Hamza <120500474+hamza-bangash@users.noreply.github.com>
2023-07-14 05:25:31 +0000

    commit variables and datatype

---
+++ .devcontainer/javascript_basic/datatype.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all js code as never versoion
+
+
+let name = "hamza";  // string
+let age = 20;    // number
+let bol = true; //boolean
+ //null  standalone value
+ // undefined  not assign value yet
+ //symbol  unique
+
+ //object
+ console.log(typeof(name));  // typeof method is used to find the data of a given variable
+
---
+++ .devcontainer/javascript_basic/variables.js
@@ -0,0 +1,21 @@
+const accountId= 12345; 
+let accountEmail = "hamzalalsahib@gmail.com";
+var accountPassword = "12345";
+accountCity = "peshwar"; // this is not perferable eay
+
+// accountId = 5; not allowed
+
+console.log(accountId);
+
+accountEmail = "hhkdhd";
+accountPassword = "erui8d";
+accountCity = "kohat";
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+
+/* 
+prefer not use var 
+bescause of issue in scope 
+*/
+
+let check ; // by  defult javascript  assigne undefine value
